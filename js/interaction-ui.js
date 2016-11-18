49329e83a2ebd8ddbefcf6fe6626b510db392812
diego migliavacca <spookyisland@hotmail.it>
2016-11-18 16:25:19 +0100

    sp7 geogate client

---
+++ js/interaction-ui.js
@@ -0,0 +1,519 @@
+﻿/**
+ * @author Diego Migliavacca (https://github.com/diegomigliavacca)
+ * @copyright SP7 Ritmare (http://www.ritmare.it)
+ */
+
+var widgetOptions = {
+	color : function(string) {
+		switch (string) {
+		case "Image":
+			return "#004998";
+			break;
+		case "Text":
+			return "#025f54";
+			break;
+		case "Plot":
+			return "#f6cd1d";
+			break;
+		case "Map":
+			return "#97b0e6";
+			break;
+		default:
+			return "#ffffff";
+		}
+	},
+	image : function(string) {
+		switch (string) {
+		case "Image":
+			return "http://cigno.ve.ismar.cnr.it/uploaded/thumbs/map-28bc6852-6871-11e4-80d1-005056bd48e7-thumb.png";
+			break;
+		case "Text":
+			return "http://cigno.ve.ismar.cnr.it/uploaded/thumbs/002788ceb5d5a5be6ecba8a0f2a4fe82.png";
+			break;
+		case "Plot":
+			return "http://cigno.ve.ismar.cnr.it/uploaded/thumbs/004fb0a385a834aa31b03351f4eb1e1b.png";
+			break;
+		case "Map":
+			return "http://cigno.ve.ismar.cnr.it/maps/1247/embed";
+			break;
+		default:
+			return "";
+		}
+	},
+	icon : function(string) {
+		switch (string) {
+		case "Image":
+			return "glyphicon glyphicon-picture text-img";
+			break;
+		case "Text":
+			return "icon-write text-img";
+			break;
+		case "Plot":
+			return "fa fa-area-chart text-img";
+			break;
+		case "Map":
+			return "imoon imoon-map text-img";
+			break;
+		default:
+			return "";
+		}
+	}
+};
+
+var storage = {
+	check : function() {
+		if ( typeof (Storage) == "undefined") {
+			$("#browser_not_supported").dialog({
+				buttons : {
+					OK : function() {
+						$(this).dialog("close");
+					}
+				},
+				modal : true,
+				show : "blind",
+				hide : "explode"
+			});
+		}
+	},
+	storeWidget : function(position, id, xy, name, ws) {
+		var widget = {
+			position : position,
+			id : id,
+			xy : xy,
+			name : name,
+			workspace : ws
+		};
+		var record = JSON.stringify(widget);
+		storageType().setItem(position + ws, record);
+	},
+	countWidgets : function() {
+		var length = 0;
+		for (var i in storageType()) {
+			if ($.isNumeric(i.substring(0, i.length - 3))) {
+				length = length + 1;
+			};
+		};
+		return length;
+	},
+	countWidgetsByName : function(name) {
+		var count = 0;
+		for (var i in window.storageType()) {
+			val = storageType().getItem(i);
+			if (val != null && $.isNumeric(i.substring(0, i.length - 3)) && i.substr(1) == storageType().getItem("workspace")) {
+				if (JSON.parse(val).name == name) {
+					count = count + 1;
+				};
+			};
+		};
+		return count;
+	},
+	countWidgetsInWs : function() {
+		var length = 0;
+		for (var i in window.storageType()) {
+			if ($.isNumeric(i.substring(0, i.length - 3)) && i.substr(1) == storageType().getItem("workspace")) {
+				length = length + 1;
+			};
+		};
+		return length;
+	},
+	getWidgetByPosition : function(pos) {
+		return JSON.parse(storageType().getItem(pos + storageType().getItem("workspace")));
+	},
+	getWidgetById : function(id) {
+		var i = 0;
+		while (i < storage.countWidgets()) {
+			i++;
+			var widget = storage.getWidgetByPosition(i);
+			if (widget != null && widget.workspace == storageType().getItem("workspace")) {
+				if (widget.id == id) {
+					return widget;
+				};
+			};
+		};
+	},
+	reloadWidgets : function() {
+		/*	for (var i = 0; i < storageType().length; i++) {
+		 var item = storageType().getItem(storageType().key(i));
+		 console.log(item);
+		 }; */
+
+		var i = 0;
+		while (i < storage.countWidgets()) {
+			i++;
+			var widget = storage.getWidgetByPosition(i);
+			if (widget != null && widget.workspace == storageType().getItem("workspace") && widget.name != 0) {
+				if (widget.position == 1) {
+					$('.head-b1 span').first().text(widget.name);
+					$('.menu-f1 span').removeClass().addClass(widgetOptions.icon(widget.name));
+				};
+				$('#draggable' + i).attr("name", widget.name);
+				$('#draggable' + i).find("embed").attr("src", widgetOptions.image(widget.name));
+				$('#draggable' + i).css("background-color", widgetOptions.color(widget.name));
+				$('#draggable' + i).css("visibility", "visible");
+				utils.appendToWidgetAttivi(widget.name);
+				$('#' + widget.name).prev().attr("data-badge2", storage.countWidgetsByName(widget.name) + "x");
+			} else {
+				$('#draggable' + i).css("visibility", "hidden");
+			};
+		};
+	}
+};
+
+var utils = {
+	appendToWidgetAttivi : function(str) {
+		if ($('#widget-attivi .icon-title').text() != str && $('#widget-attivi .icon-title').text().indexOf(str) < 0) {
+			$('#widget-attivi').append('<a href="#"> <span class="badge" data-badge="0"></span> <span class="badge2" data-badge2="' + storage.countWidgetsByName(str) + 'x"></span> <span id="' + str + '" class="awidg ' + widgetOptions.icon(str) + '"></span><br /><span class="icon-title">' + str + '</span> </a>');
+		}
+	},
+	removeFromWidgetAttivi : function() {
+		$('#widget-attivi').find('.badge2').each(function() {
+			if ($(this).attr("data-badge2") == "0x") {
+				$(this).parent().remove();
+			}
+		});
+	},
+	addBadgeValue : function(str) {
+		$('#widget-attivi').find('.badge2').each(function() {
+			$(this).attr("data-badge2", storage.countWidgetsByName($(this).parent().find(".awidg").attr("id")) + "x");
+		});
+		$('#wrap-icons').find('.badge2').each(function() {
+			$(this).attr("data-badge2", storage.countWidgetsByName($(this).parent().find(".cwidg").attr("id")) + "x");
+		});
+		utils.removeFromWidgetAttivi();
+	},
+	findMissingId : function() {
+		var ids = ["draggable1", "draggable2", "draggable3", "draggable4", "draggable5"];
+		for (var i = 0; i < ids.length; i++) {
+			var widget = storage.getWidgetById(ids[i]);
+			if (widget == null || widget.name == 0) {
+				return ids[i];
+			}
+		};
+	}
+};
+
+function storageType() {
+	if (localStorage.getItem("session") == 0) {
+		return sessionStorage;
+	} else {
+		return localStorage;
+	}
+};
+
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+		vars[key] = value;
+	});
+	return vars;
+};
+
+function addWidget(clickedId) {
+	//add a new widget by drag & drop
+	var i = 6;
+	while (--i) {
+		var formerWidget = storage.getWidgetByPosition(i - 1);
+		if (formerWidget != null) {
+			var actualWidget = storage.getWidgetByPosition(i);
+			if (actualWidget == null) {
+				id = utils.findMissingId();
+			} else {
+				id = actualWidget.id;
+			}
+			$('#' + id).find("embed").attr("src", widgetOptions.image(formerWidget.name));
+			$('#' + id).attr("name", formerWidget.name);
+			$('#' + id).css("background-color", widgetOptions.color(formerWidget.name));
+			storage.storeWidget(i, id, $("#" + id).position(), formerWidget.name, storageType().getItem("workspace"));
+			if ($('#' + id).css("visibility") == "hidden") {
+				$('#' + id).css("visibility", "visible");
+			};
+			if (formerWidget.name == 0) {
+				$('#' + actualWidget.id).css("visibility", "hidden");
+			};
+		};
+	};
+
+	$('.head-b1 span').first().text(clickedId);
+	$('.menu-f1 span').removeClass().addClass(widgetOptions.icon(clickedId));
+
+	//	if (storageType().getItem("1" + storageType().getItem("workspace")) !== null) {
+	//		var nId = storage.getWidgetByPosition("1").id;
+	//	} else {
+	if ($("#draggable1").position().top == 0 && $("#draggable1").position().left == 0) {
+		var nId = "draggable1";
+	} else if ($("#draggable2").position().top == 0 && $("#draggable2").position().left == 0) {
+		var nId = "draggable2";
+	} else if ($("#draggable3").position().top == 0 && $("#draggable3").position().left == 0) {
+		var nId = "draggable3";
+	} else if ($("#draggable4").position().top == 0 && $("#draggable4").position().left == 0) {
+		var nId = "draggable4";
+	} else if ($("#draggable5").position().top == 0 && $("#draggable5").position().left == 0) {
+		var nId = "draggable5";
+	}
+	//	};
+
+	if ($('#' + nId).css("visibility") == "hidden") {
+		$('#' + nId).css("visibility", "visible");
+	};
+	$('#' + nId).find("embed").attr("src", widgetOptions.image(clickedId));
+	$('#' + nId).attr("name", clickedId);
+	$('#' + nId).css("background-color", widgetOptions.color(clickedId));
+	storage.storeWidget(1, nId, {
+		"top" : 0,
+		"left" : 0
+	}, clickedId, storageType().getItem("workspace"));
+
+	utils.appendToWidgetAttivi(clickedId);
+	utils.addBadgeValue();
+};
+
+$(function() {
+	storage.check();
+	sessionStorage.clear();
+	storageType().setItem("workspace", "ws1");
+	if (localStorage.getItem("session") == null) {
+		localStorage.setItem("session", 0);
+	};
+	if (localStorage.getItem("session") == 1) {
+		storage.reloadWidgets(storageType().getItem("workspace"));
+	};
+	// localStorage.clear();
+	if (window.location.href.indexOf("code") > -1) {
+		$.ajax({
+			headers : {
+				'Accept' : 'application/json',
+				'Content-Type' : 'application/x-www-form-urlencoded'
+			},
+			method : "POST",
+			dataType : 'json',
+			//url : "https://pub.orcid.org/oauth/token?client_id=APP-KCZPVLP7OMJ1P69L&client_secret=bec72dc4-c107-4fd7-8cda-12ac18ff5fd9&grant_type=authorization_code&code=" + getUrlVars()["code"] + "&redirect_uri=http://localhost/sp7-geogate-client"
+			url : "https://pub.orcid.org/oauth/token?client_id=APP-KCZPVLP7OMJ1P69L&client_secret=bec72dc4-c107-4fd7-8cda-12ac18ff5fd9&grant_type=authorization_code&code=" + getUrlVars()["code"] + "&redirect_uri=http://155.253.20.62/client"
+		}).done(function(msg) {
+			console.log("ORCID: " + msg.orcid);
+		});
+
+		if (localStorage.getItem("session") == 0) {
+			$("#reload_session").dialog({
+				closeOnEscape : false,
+				buttons : {
+					OK : function() {
+						localStorage.setItem("session", 1);
+						$(this).dialog("close");
+						storage.reloadWidgets(storageType().getItem("workspace"));
+					},
+					Annulla : function() {
+						localStorage.setItem("session", 0);
+						$('[id^=draggable]').css("visibility", "hidden");
+						$('#widget-attivi').find('.badge2').attr("data-badge2", "0x");
+						$('#wrap-icons').find('.badge2').attr("data-badge2", "0x");
+						sessionStorage.clear();
+						$(this).dialog("close");
+					}
+				},
+				modal : true,
+				show : "blind",
+				hide : "explode"
+			});
+		};
+		$("#user-login").removeClass("fa fa-user").addClass("fa fa-sign-out");
+		$("#user-title").text("Logout");
+	} else {
+		$("#user-login").removeClass("fa fa-sign-out").addClass("fa fa-user");
+		$("#user-title").text("Login");
+	};
+
+	$('.block').hover(function() {
+		if (storage.getWidgetById(this.id) != undefined && storage.getWidgetById(this.id).position != "1") {
+			$('.widget-overlay', this).show();
+		}
+	}, function() {
+		$('.widget-overlay', this).hide();
+	});
+});
+
+$(function() {
+	$('[id^=draggable]').droppable({
+		tolerance : "pointer",
+		activate : function(event, ui) {
+			draggableClass = ui.draggable.attr('class').substring(0, 5);
+			switch (draggableClass) {
+			case "cwidg":
+				$("#" + ui.helper.context.id).css("zIndex", 999);
+				break;
+			case "block":
+				$("#" + ui.draggable.context.id).css("zIndex", 999);
+				draggedElement = ui.draggable.context.id.substring(9) - 1;
+				bloccoPos = new Array();
+				bloccoWidth = new Array();
+				bloccoHeight = new Array();
+				ifbHeight = new Array();
+				var i = 0;
+				while (i < 5) {
+					i++;
+					bloccoPos.push($('#draggable' + i).position());
+					bloccoWidth.push($('#draggable' + i).width());
+					bloccoHeight.push($('#draggable' + i).height());
+					ifbHeight.push($('#draggable' + i).find('.ifb').height());
+				};
+				break;
+			};
+		},
+		drop : function(event, ui) {
+			switch (draggableClass) {
+			case "cwidg":
+				addWidget(ui.draggable.context.id);
+				break;
+			case "block":
+				//change the widget position by drag & drop
+				var targetElement = event.target.id.substring(9) - 1;
+				var targetId = event.target.id;
+				var draggedId = ui.draggable.context.id;
+				if (storage.getWidgetById(targetId) !== undefined) {
+					$("#" + draggedId).draggable("option", "revert", false);
+					var targetPosition = storage.getWidgetById(targetId).position;
+					var draggedPosition = storage.getWidgetById(draggedId).position;
+					var targetXy = storage.getWidgetById(targetId).xy;
+					var draggedXy = storage.getWidgetById(draggedId).xy;
+					$("#" + draggedId).animate(bloccoPos[targetElement]).css("position", "absolute");
+					$("#" + targetId).animate(bloccoPos[draggedElement]);
+					$("#" + draggedId).width(bloccoWidth[targetElement]);
+					$("#" + draggedId).height(bloccoHeight[targetElement]);
+					$("#" + targetId).width(bloccoWidth[draggedElement]);
+					$("#" + targetId).height(bloccoHeight[draggedElement]);
+					$("#" + draggedId).find('.ifb').height(ifbHeight[targetElement]);
+					$("#" + targetId).find('.ifb').height(ifbHeight[draggedElement]);
+					if (targetPosition == 1) {
+						$('#bar').prependTo("#" + draggedId + " .content");
+						$('.head-b1 span').first().text(ui.draggable.attr('name'));
+						$('.menu-f1 span').removeClass().addClass(widgetOptions.icon(ui.draggable.attr('name')));
+					} else if (draggedPosition == 1) {
+						$('#bar').prependTo("#" + targetId + " .content");
+						$('.head-b1 span').first().text($(event.target).attr('name'));
+						$('.menu-f1 span').removeClass().addClass(widgetOptions.icon($(event.target).attr('name')));
+					}
+					storage.storeWidget(targetPosition, draggedId, targetXy, ui.draggable.attr('name'), storageType().getItem("workspace"));
+					storage.storeWidget(draggedPosition, targetId, draggedXy, $(event.target).attr('name'), storageType().getItem("workspace"));
+					var i = 0;
+					while (i < 6) {
+						i++;
+						if (i - 1 == draggedElement || i - 1 == targetElement) {
+							continue;
+						};
+						$('#draggable' + i).animate(bloccoPos[i - 1]).css("position", "absolute");
+						$('#draggable' + i).width(bloccoWidth[i - 1]);
+						$('#draggable' + i).height(bloccoHeight[i - 1]);
+					};
+					break;
+				} else {
+					$("#" + draggedId).draggable("option", "revert", true);
+				}
+			}
+		},
+		deactivate : function(event, ui) {
+			$("#" + ui.draggable.context.id).css("zIndex", 99);
+		}
+	});
+
+	$('.cwidg').draggable({
+		cursor : "crosshair",
+		helper : "clone",
+		revert : "invalid"
+	});
+
+	$('.block').draggable({
+		cursor : "move",
+		revert : "invalid"
+	});
+});
+
+$(function() {
+	$('.cwidg').click(function() {
+		addWidget($(this).attr('id'));
+	});
+
+	$('#user-press-login').click(function() {
+		if (window.location.href.indexOf("code") > -1) {
+			localStorage.setItem("session", 0);
+			//window.location.href = "http://localhost/sp7-geogate-client";
+			window.location.href = "http://155.253.20.62/client";
+		} else {
+			$("#user_login").dialog({
+				modal : true,
+				show : "blind",
+				hide : "explode"
+			});
+		}
+	});
+
+	$('.full-f1').click(function() {
+		$(this).closest('.block').toggleClass('fullscreen');
+	});
+
+	$('.close-f1').click(function() {
+		var widget = storage.getWidgetByPosition(1);
+		storageType().removeItem("1" + storageType().getItem("workspace"));
+		utils.addBadgeValue();
+		var i = 0;
+		while (i < 5) {
+			i++;
+			var nextWidget = storage.getWidgetByPosition(i + 1);
+			if (nextWidget != null) {
+				if (i != 1) {
+					widget = storage.getWidgetByPosition(i);
+				} else {
+					if (nextWidget.name == 0) {
+						$('.head-b1 span').first().text("");
+					} else {
+						$('.head-b1 span').first().text(nextWidget.name);
+					}
+					$('.menu-f1 span').removeClass().addClass(widgetOptions.icon(nextWidget.name));
+				}
+				$('#' + widget.id).find("embed").attr("src", widgetOptions.image(nextWidget.name));
+				$('#' + widget.id).attr("name", nextWidget.name);
+				$('#' + widget.id).css("background-color", widgetOptions.color(nextWidget.name));
+				storage.storeWidget(i, widget.id, widget.xy, nextWidget.name, storageType().getItem("workspace"));
+				if (nextWidget.name == 0) {
+					$('#' + widget.id).css("visibility", "hidden");
+				}
+			} else {
+				if (i == 1) {
+					$('.head-b1 span').first().text("");
+					$('#' + widget.id).css("visibility", "hidden");
+				}
+			};
+		};
+		if (storage.countWidgetsInWs() > 0) {
+			$('#' + storage.getWidgetByPosition(storage.countWidgetsInWs()).id).css("visibility", "hidden");
+			var position = storage.getWidgetByPosition(storage.countWidgetsInWs()).position;
+			var id = storage.getWidgetByPosition(storage.countWidgetsInWs()).id;
+			var ws = storage.getWidgetByPosition(storage.countWidgetsInWs()).workspace;
+			var value = {
+				position : position,
+				id : id,
+				xy : 0,
+				name : 0,
+				workspace : ws
+			};
+			storageType().setItem(storage.countWidgetsInWs() + storageType().getItem("workspace"), JSON.stringify(value));
+		};
+		utils.addBadgeValue();
+
+		for (var i = 0; i < storageType().length; i++) {
+			var item = storageType().getItem(storageType().key(i));
+			console.log(item);
+		};
+	});
+
+	$('.ws').click(function() {
+		var id = String($(this).attr("id"));
+		storageType().setItem("workspace", id);
+		storage.reloadWidgets(id);
+		utils.addBadgeValue();
+	});
+});
+
+function openORCID() {
+	//window.location.href = 'https://orcid.org/oauth/authorize?client_id=APP-KCZPVLP7OMJ1P69L&response_type=code&scope=/authenticate&redirect_uri=http://localhost/sp7-geogate-client&show_login=true';
+	window.location.href = 'https://orcid.org/oauth/authorize?client_id=APP-KCZPVLP7OMJ1P69L&response_type=code&scope=/authenticate&redirect_uri=http://155.253.20.62/client&show_login=true';
+	$("#user_login").dialog("close");
+};
---
+++ js/ui.js
@@ -0,0 +1,143 @@
+$('a').click(function(e) {
+	e.preventDefault();
+});
+
+$('.menu-f1').click(function() {
+	if ($('#blocco-menu-f1').hasClass("espanso")) {
+		$('#blocco-menu-f1').fadeOut(500, function() {
+			$('#blocco-menu-f1').removeClass('espanso');
+		});
+	} else {
+
+		$('#blocco-menu-f1').fadeIn(500, function() {
+			$('#blocco-menu-f1').addClass('espanso');
+		});
+	}
+});
+
+$('.actmenu').click(function() {
+	$('#menudx').toggleClass('opened');
+	$(this).toggleClass('close');
+	$('.imgopen').toggleClass('nascondi');
+	$('.imgclose').toggleClass('nascondi');
+});
+$('.actmenu-top').click(function() {
+	$('#menutop').toggleClass('opened');
+	$(this).toggleClass('close');
+	$('.imgopen-top').toggleClass('nascondi');
+	$('.imgclose-top').toggleClass('nascondi');
+});
+
+$('.block').click(function() {
+	var bclass = $(this).attr('rel');
+	$('#logo_r').attr('class', bclass);
+});
+
+function get_browser_info() {
+	var ua = navigator.userAgent,
+	    tem,
+	    M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+	if (/trident/i.test(M[1])) {
+		tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+		return {
+			name : 'IE',
+			version : (tem[1] || '')
+		};
+	}
+	if (M[1] === 'Chrome') {
+		tem = ua.match(/\bOPR\/(\d+)/);
+		if (tem != null) {
+			return {
+				name : 'Opera',
+				version : tem[1]
+			};
+		}
+	}
+	M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+	if (( tem = ua.match(/version\/(\d+)/i)) != null) {
+		M.splice(1, 1, tem[1]);
+	}
+	return {
+		name : M[0],
+		version : M[1]
+	};
+};
+
+var browser = get_browser_info();
+// browser.name = 'Chrome'
+// browser.version = '40'
+
+var vh = window.innerHeight;
+var vw = window.innerWidth;
+
+if (browser.name == "Safari" && browser.version < 9 && vw > 800) {
+	var b1w = (vw - 50) * 0.618181818181818;
+
+	var b2h = vh * 0.617647058823529;
+
+	var b3w = vw * 0.236363636363636;
+
+	var b4h = vh * 0.235294117647059;
+
+	var b5w = vw * 0.090909090909091;
+
+	$('.blocco-2').css({
+		"height" : b2h
+	});
+	$('.blocco-1').css({
+		"width" : b1w
+	});
+	$('.blocco-3').css({
+		"width" : b3w
+	});
+	$('.blocco-4').css({
+		"height" : b4h
+	});
+	$('.blocco-5').css({
+		"width" : b5w
+	});
+};
+
+$(window).on('resize', function() {
+	var vh = window.innerHeight;
+	var vw = window.innerWidth;
+
+	if (browser.name == "Safari" && browser.version < 9 && vw > 800) {
+
+		var b1w = (vw - 50) * 0.618181818181818;
+
+		var b2h = vh * 0.617647058823529;
+
+		var b3w = vw * 0.236363636363636;
+
+		var b4h = vh * 0.235294117647059;
+
+		var b5w = vw * 0.090909090909091;
+
+		$('.blocco-2').css({
+			"height" : b2h
+		});
+		$('.blocco-1').css({
+			"width" : b1w
+		});
+		$('.blocco-3').css({
+			"width" : b3w
+		});
+		$('.blocco-4').css({
+			"height" : b4h
+		});
+		$('.blocco-5').css({
+			"width" : b5w
+		});
+	} else if (browser.name == "Safari" && browser.version < 9 && vw < 800) {
+		$('.blocco-1').css({
+			"width" : "100%"
+		});
+		$('.blocco-3').css({
+			"width" : "100%"
+		});
+		$('.blocco-5').css({
+			"width" : "100%"
+		});
+	}
+});
