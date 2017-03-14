a129ed92272e55c44b4b7b2b22eb9d447a0242cd
diegomigliavacca <spookyisland@hotmail.it>
2017-03-14 17:17:45 +0100

    added default menus to widgets

--- js/utils.js
+++ js/utils.js
@@ -209,6 +209,8 @@ var utils = {
 				$('#dropdown-menu').append("<li><a href='#'>" + arr[i] + "</a></li>");
 			};
 		};
+		$('#dropdown-menu').append("<li><a href='#'>Toggle fullscreen</a></li>");
+		$('#dropdown-menu').append("<li><a href='#'>Close widget</a></li>");
 	},
 	endpoint_query : "http://geogate.sp7.irea.cnr.it/fuseki/portal/query",
 	endpoint_update : "http://geogate.sp7.irea.cnr.it/fuseki/portal/update"
